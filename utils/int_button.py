5dc320848f0f5597ed855084722fe4238e0f2c27
Abduguru <112319829+Abduguru@users.noreply.github.com>
2024-06-26 11:40:00 +0100

    done

---
+++ utils/int_button.py
@@ -0,0 +1,28 @@
+import flet as ft 
+
+
+def incr_btn(e, text):
+    text.value = str(float(text.value) + 1.0)
+    e.page.update()
+
+def decr_btn(e, text):
+    text.value = str(float(text.value) - 1.0)
+    e.page.update()
+
+def Stepper():
+    text =  ft.TextField(
+        text_align="center",
+        value="0",
+        keyboard_type=ft.KeyboardType.NUMBER,
+        label="inch",
+        width=150,
+        height=60,
+        prefix=ft.IconButton(ft.icons.ARROW_UPWARD, icon_color=ft.colors.BLACK, on_click=lambda e: incr_btn(e, text)),
+        suffix=ft.IconButton(ft.icons.ARROW_DOWNWARD, icon_color=ft.colors.BLACK, on_click=lambda e: decr_btn(e, text)),
+        border_radius=10,
+        show_cursor=False,
+        color="black",
+        input_filter=ft.NumbersOnlyInputFilter()
+)
+    return text
+
